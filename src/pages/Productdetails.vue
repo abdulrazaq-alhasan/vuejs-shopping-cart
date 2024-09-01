3bd116065c19bce0734b49087dce42990dea3610
Abdulrazaq Alhasan <alhsn.me@gmail.com>
2024-09-01 09:37:16 +0200

    Details Page

---
+++ src/pages/Productdetails.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="row mt-2">
+        <div class="col-md-6">
+            <img width="400" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="">
+
+        </div>
+        <div class="col-md-6 mt-5">
+            <h3>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <h6>Price: 200 $</h6>
+            <div class="d-flex">
+                <div class="btn-group me-auto">
+                    <button type="button" class="btn btn-outline-primary">Primary</button>
+                    <button type="button" class="btn btn-outline-primary">0</button>
+                    <button type="button" class="btn btn-outline-primary">Primary</button>
+
+                </div>
+                <button class="btn btn-outline-primary">Primary</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createWebHistory, createRouter } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Products from '../pages/Products.vue'
 import Cart from '../pages/Cart.vue'
+import Productdetails from '../pages/Productdetails.vue'
+
 
 
 
@@ -10,6 +12,7 @@ import Cart from '../pages/Cart.vue'
 const routes = [
     { path: '/', component: Home },
     { path: '/products', component: Products },
+    { path: '/details', component: Productdetails },
     { path: '/cart', component: Cart },
 ]
 
