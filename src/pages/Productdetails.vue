ddcd65f442451901b9455ec3c8e1ff9511e775f7
Abdulrazaq Alhasan <alhsn.me@gmail.com>
2024-09-14 10:42:48 +0200

    uppdate

--- src/pages/Productdetails.vue
+++ src/pages/Productdetails.vue
@@ -1,29 +1,30 @@
 <script setup>
-import axios from 'axios';
-import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 
-const route = useRoute()
-const id = route.params.id
+const route = useRoute();
+const id = route.params.id;
 
-const product = ref({})
+const product = ref({});
 
 const getProduct = async () => {
     await axios(`https://fakestoreapi.com/products/${id}`)
-        .then(response => {
-            console.log(response.data)
-            product.value = response.data
-        }).catch(err => console.log(err))
-}
+        .then((response) => {
+            console.log(response.data);
+            product.value = response.data;
+        })
+        .catch((err) => console.log(err));
+};
 
-onMounted(() => getProduct())
+onMounted(() => getProduct());
+
 </script>
 
 <template>
     <div class="row mt-5">
         <div class="col-md-6">
-            <img width="350" :src="product.image" alt="">
-
+            <img width="350" :src="product.image" alt="" />
         </div>
         <div class="col-md-6">
             <h3>{{ product.title }}</h3>
@@ -34,7 +35,6 @@ onMounted(() => getProduct())
                     <button type="button" class="btn btn-primary">-</button>
                     <button type="button" class="btn btn-outline-primary">0</button>
                     <button type="button" class="btn btn-primary">+</button>
-
                 </div>
                 <button class="btn btn-primary btn-sm">Add To Cart</button>
             </div>
@@ -42,6 +42,4 @@ onMounted(() => getProduct())
     </div>
 </template>
 
-<style lang="sass" scoped>
-
-</style>
+<style lang="sass" scoped></style>
--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,35 +1,34 @@
 <script setup>
-import { computed } from 'vue';
-import { useProductsStore } from '../store/products';
-import { useRouter } from 'vue-router';
+import { computed } from "vue";
+import { useProductsStore } from "../store/products";
+import { useRouter } from "vue-router";
 
-const productsStore = useProductsStore()
+const productsStore = useProductsStore();
 
-const products = computed(() => productsStore.productsList)
+const products = computed(() => productsStore.productsList);
 
-const router = useRouter()
+const router = useRouter();
 </script>
 
 <template>
     <div class="row">
         <div class="col" v-for="product in products" :key="product.id">
-            <div class="card mt-3" style="width: 16rem;">
-                <img :src="product.image" class="card-img-top" alt="product.title" style="height: 200px;">
+            <div class="card mt-3" style="width: 16rem">
+                <img :src="product.image" class="card-img-top" alt="product.title" style="height: 200px" />
                 <div class="card-body">
                     <h5 class="card-title">{{ product.title.slice(0, 17) }}...</h5>
                     <p class="card-text my-0">{{ product.category }}</p>
                     <p class="card-text">Price: {{ product.price }} $</p>
                     <div class="d-flex justify-content-between">
-                        <button @click="router.push(`/details/${product.id}`)" class="btn btn-primary btn-sm">Details</button>
+                        <button @click="router.push(`/details/${product.id}`)" class="btn btn-primary btn-sm">
+                            Details
+                        </button>
                         <button class="btn btn-success btn-sm">Add To Cart</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </template>
 
-<style lang="sass" scoped>
-
-</style>
+<style lang="sass" scoped></style>
